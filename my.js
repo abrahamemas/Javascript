f96d2c892da52057e0e0acaffaf373b459d15cb9
abrahamemas <abrahamemas@gmail.com>
2021-08-17 15:55:38 +0100

    third commit

--- my.js
+++ my.js
@@ -237,14 +237,14 @@ console.log(Emas.Sumofbill)
          
      }
  }*/
- var scores, roundscores,activeplayers,dice;
+ var scores, roundscores,activeplayer,dice;
  scores = [0,0];
  roundscores = 0;
- activeplayers = 0;
+ activeplayer = 0;
  document.getElementById('score--0').textContent=0;
  document.getElementById('score--1').textContent=0;
  document.getElementById('current--0').textContent=0;
- document.getElementById('current--1').textConten = 0;
+ document.getElementById('current--1').textContent = 0;
 
  document.querySelector(".btn--roll").addEventListener('click',function(){
      var dice = Math.floor(Math.random()*6)+1;
@@ -253,27 +253,38 @@ console.log(Emas.Sumofbill)
      diceDom.src = 'dice-'+ dice + '.png';
      if (dice !== 1){
          roundscores += dice;
-         document.querySelector('#current--' + activeplayers).textContent = roundscores;
+         document.querySelector('#current--' + activeplayer).textContent = roundscores;
         
     
-     }else {
-      activeplayers===0 ? activeplayers = 1 : activeplayers = 0;
-      roundscores=0;
-      document.getElementById('current--0').textContent=0;
-      document.getElementById('current--1').textContent=0
+     }else { 
+        newplayer();
 
      
      }
       document.querySelector(".btn--hold").addEventListener('click',function(){
-      scores[activeplayers] += roundscores;
-      document.querySelector('#score--' + activeplayers).textContent = scores[activeplayers];
-     
+      scores[activeplayer] += roundscores;
+      document.querySelector('#score--' + activeplayer).textContent = scores[activeplayer];
+      var winner;
+      if (scores[activeplayer]>= 30){
+          document.querySelector('#name--'+ activeplayer).textContent = 'WINNER';
+      }else {
+          newplayer ();
+      }
+
+      newplayer();
     
     
 
 
       })
+function newplayer (){
+    activeplayer === 0 ? activeplayer = 1 : activeplayer = 0;
+    roundscores = 0;
+    document.getElementById('current--0').textContent=0;
+    document.getElementById('current--1').textContent = 0;
 
+
+}
 
     
      
